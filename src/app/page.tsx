f0f0230043c05ad4f883eabe438a3fd6915d897a
Retr0 <isaquemedeirosx@gmail.com>
2025-02-11 16:51:33 -0300

    :sparkles: Página padrão mudada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import FormNotas from "@/components/FormElements/FormNotas";
 
 export const metadata: Metadata = {
   title:
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <DefaultLayout>
-        <ECommerce />
+        <FormNotas />
       </DefaultLayout>
     </>
   );
